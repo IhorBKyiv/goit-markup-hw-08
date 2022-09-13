e95e7240460fb18bf5c536b4f7a18025cd3b1ed9
Ihor Burkytsia <burkitsa.i@gmail.com>
2022-09-13 17:57:53 +0300

    Made-Mobile-menu

---
+++ js/mobile-menu.js
@@ -0,0 +1,5 @@
+document.querySelector('.mobile-btn').addEventListener('click',()=> {
+document.querySelector('.menu-mobile').classList.toggle('active');
+document.querySelector('.mobile-btn').classList.toggle('active');
+document.body.classList.toggle('overflow');
+});
